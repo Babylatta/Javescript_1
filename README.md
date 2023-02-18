ac6dd059d8097a994ed0a98fa089b1fe632a42aa
Babylatta <111289746+Babylatta@users.noreply.github.com>
2023-02-18 17:12:02 +0900

    Creating Calculator

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript_1
+
+Learning Javescript!
---
+++ app.js
@@ -0,0 +1,23 @@
+const calculator = {
+    add:function (a,b) {
+        console.log(a+b);
+    },
+    minus:function (a,b) {
+        console.log(a-b);
+    },
+    multiply:function (a,b) {
+        console.log(a*b);
+    },
+    div:function (a,b) {
+        console.log(a/b);
+    },
+    power:function (a,b) {
+        console.log(a**b);
+    },
+};
+
+calculator.add(5,1);
+calculator.minus(100,1);
+calculator.multiply(7,1000);
+calculator.div(16,4);
+calculator.power(5,3); 
