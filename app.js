bd4f47a7e38ad3c945a1925ff0631b5af5d503c2
Babylatta <111289746+Babylatta@users.noreply.github.com>
2023-02-19 23:35:16 +0900

    clickmecolorchanging

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    h1.style.color="blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
-
-function handleMouseEnter() {
-    h1.innerText="Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText="Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundcolor="tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOOD");
-}
-
 
 
 h1.addEventListener("click",handleTitleClick);
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("online",handleWindowOnline);
-window.addEventListener("offline",handleWindowOffline);
